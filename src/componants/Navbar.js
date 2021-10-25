e81e6edbb67e2382e442a6dbb6dcbf49c74ba5ad
Abhishek Tomar <61470005+Thakur-15x@users.noreply.github.com>
2021-10-25 11:48:06 +0530

    new commit

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -1,29 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import logo from '../componants/logo/Logo.png'
-import './Nav.css'
+import React, { useState, useEffect } from "react";
+import logo from "../componants/logo/Logo.png";
+import "./Nav.css";
 
 function Navbar() {
-    const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener("scroll", ()=>{
-            if(window.scrollY > 100){
-                handleShow(true);
-            } else handleShow(false);
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
-    return (
-        <div className={`navbar ${show && "navblack"}`}>
-        <img src={logo} alt="" srcSet="" />
-        <button className="gener">Sign In</button>
-        </div>
-        
-        
-    )
+  return (
+    <div className={`navbar ${show && "navblack"}`}>
+      <img src={logo} alt="" srcSet="" />
+      <button className="gener">Sign In</button>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
